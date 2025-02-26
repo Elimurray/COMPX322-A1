753614950ba7aff8b7724a4d58c75c783622efb6
Elimurray <eli.murr04@gmail.com>
2025-02-27 12:39:42 +1300

    added weather api

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 console.log("wialohdoiahd");
+
+async function getSomething() {
+    const response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + 37.2 + "&lon=" + 175.11 + "&appid=" + "-")
+        .then((data) => data.json())
+    // console.log(await response.json())
+    return response
+}
+
+getSomething().then((data) => {
+    console.log(data)
+    let lol = ""
+    console.log("Temp: ", data["main"]["temp"])
+    let something = document.getElementById("test")
+    something.addEventListener("click", () => {
+        console.log(lol)
+    })
+})
+
+console.log("this is after function call")
+
+// async
+// fetch
+// promise
+
+// element
